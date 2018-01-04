9504f9c0233f62656b84b184d71999c5d0303b35
justineDelo <ju.nyc@hotmail.fr>
2018-01-04 16:52:26 +0100

    less pseudo code

--- regrouped_code/scouting.c
+++ regrouped_code/scouting.c
@@ -5,15 +5,27 @@ it will turn around the obstacle then avoid it and put the robot towards the ini
 */
 	int end=0;	
 	int distance = 5; //in cm
-	rotate_car(90, 'R', SPEED_CIRCULAR)
+	float x_init = x_position;
+	float y_init=y_position;
+	rotate_car(90, 'R', SPEED_CIRCULAR);
 	for(int side=1;	side<=5; side++){ //5 in order to do the missing part of the first side at the end
 
 		while(!end){
 			
-			move(velocity,distance/velocity,0,'F');
+			move(SPEED_LINEAR,distance/velocity,0,'F');
 			rotate_car(90, 'L', SPEED_CIRCULAR);
 			if(!detect_obstacle()){
 				end=1;
+				if(side==1){
+					xDecal=abs(x_position-x_init);
+					yDecal=abs(y_position-y_init);
+					x_init=x_position;
+					y_init = y_position;
+				}
+				if (side==2){
+					xDecal2=abs(x_position-x_init);
+					yDecal2=abs(y_position-y_init);				
+				}
 			}
 			else{
 				addToMap;
@@ -23,8 +35,8 @@ it will turn around the obstacle then avoid it and put the robot towards the ini
 
 		}
 	}
-	move(after the obstacle)
+	move(SPEED_LINEAR, max(xDecal,yDecal)/velocity, 0, 'F'); //longueur //go after the obstacle
 	rotate_car(90, 'L', SPEED_CIRCULAR);
-	move(se realigner)
+	move(SPEED_LINEAR, max(xDecal,yDecal)/velocity,0, 'F'); //largeur //realignment with initial position
 	rotate car(90, 'R', SPEED_CIRCULAR);
 }
