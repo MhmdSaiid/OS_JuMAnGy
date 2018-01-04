999fa1aa2e8d5d6610eb840c2054dbe9a5679d7b
justineDelo <ju.nyc@hotmail.fr>
2018-01-04 16:20:40 +0100

    replace obstacle_type by int

--- regrouped_code/detect_disting_functions.h
+++ regrouped_code/detect_disting_functions.h
@@ -1,7 +1,9 @@
-typedef enum obstacle_type obstacle_type;
+/*typedef enum obstacle_type obstacle_type;
 enum obstacle_type {ROBOT, UNMOVABLE, MOVABLE, BOUNDARIES, NONE};
+*/
 
 int detect_obstacle(void);
 void find_right_angle_obst(void);
 int find_obst_size(int max_size, int count);
-obstacle_type distinguish_obstacle(void);
+//obstacle_type distinguish_obstacle(void);
+int distinguish_obstacle(void);
