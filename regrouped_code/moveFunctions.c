1f4bb5bf5168dd720f00b7355c1fbeff8ab66b28
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-07 18:12:16 +0100

    not is turning

--- regrouped_code/moveFunctions.c
+++ regrouped_code/moveFunctions.c
@@ -169,7 +169,7 @@ void rotate_car(int angle,char D, int speed_circular) //Clockwise
 	//rotate_car(angle,D,speed_circular);
 	bool turning=true;
 	float small_angle_to_turn = 10.0;
-	while(turning)
+	while(is_running())
 	{
 		ANG_VAL = read_ang();
 		printf("final_angle = %f ,ANG_VAL = %f\n",final_angle,ANG_VAL);
