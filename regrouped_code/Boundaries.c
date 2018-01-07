8a9825c8d50989646133d3fdec1e00fa76ca1d2f
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-07 16:36:05 +0100

    10 cm threshold to see a Boundaries

--- regrouped_code/Boundaries.c
+++ regrouped_code/Boundaries.c
@@ -93,10 +93,10 @@ int main (void )
 
  	while(1){
 
-	while(read_US()>10000){
+	while(read_US()>100){
 	move(SPEED_LINEAR,1000,1,'F');
 	rotate_car(90,'R',SPEED_CIRCULAR);
-	if(read_US()<10000)
+	if(read_US()<100)
 	rotate_car(90,'L',SPEED_CIRCULAR);
 	Sleep(150);
 	}
