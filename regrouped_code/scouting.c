c47aa73b1597e9e49dea0d60b3ac5a4a5cc25b71
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-11 18:56:05 +0100

    printf US Val

--- regrouped_code/scouting.c
+++ regrouped_code/scouting.c
@@ -179,6 +179,7 @@ void scouting(){
 			//addLineOf(map, (int)(floor(former_x/5)), (int)(floor(former_y/5)), (int)(floor(new_x/5)), (int)(floor(new_y/5)), EMPTY);
 			//stops when there is an obstacle or a boundary
 			//if(checkBoundary(floor(x_position/5),floor(y_position/5))) {
+			printf("US_VAL = %f\n",US_VAL);
 			if(checkBoundary( US_VAL+(x_position*cos(relative_angle)), US_VAL+(y_position*sin(relative_angle))) ){
 				//if it is a boundary according to our array
 				if(goingRight){
