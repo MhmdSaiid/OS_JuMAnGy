444c3a05c078a6734cf91d7cf534bc71e11c378b
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-13 17:09:36 +0100

    correct

--- regrouped_code/scouting.c
+++ regrouped_code/scouting.c
@@ -104,7 +104,7 @@ but after the obstacle in the same scouting line drawn
 	if ((relative_angle>170) && (relative_angle<190)) directionIndex = 0; //going left
 	bool besideObstacle = true;
 	int angleTracker = relative_angle;
-	move(SPEED_LINEAR,timeToStop,0,'B');
+	move(SPEED_LINEAR,timeToStop/2,0,'B');
 	rotate_car(90, directionsGoingRight[directionIndex], SPEED_CIRCULAR);
 	int obstacleWhileMoving;
 	uint8_t side=1;
@@ -125,8 +125,8 @@ but after the obstacle in the same scouting line drawn
 				rotate_car(90, directionsGoingRight[(directionIndex+1)%2], SPEED_CIRCULAR); //get some distance from the obstacle
 				drift = (read_ang() - relative_angle);//negative means drift to the right
 				printf("drift = %f\n",drift);
-				if(drift<0) rotate_car(drift,'L',SPEED_CIRCULAR);
-				else rotate_car(drift,'R',SPEED_CIRCULAR);
+				if(drift<0) rotate_car(abs(drift%360),'L',SPEED_CIRCULAR);
+				else rotate_car(abs(drift%360),'R',SPEED_CIRCULAR);
 				obstacleWhileMoving = move(SPEED_LINEAR,floor(timeToStop*1.5),0,'F');
 				if(obstacleWhileMoving){
 					////find_right_angle_obst();
