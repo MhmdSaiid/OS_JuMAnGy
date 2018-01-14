87d5f61ffc17c0c91de2b98799d618ef97f0bffd
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-14 19:06:48 +0100

    ANG_VAL

--- regrouped_code/moveFunctions.c
+++ regrouped_code/moveFunctions.c
@@ -205,7 +205,7 @@ void rotate_car(float angle,char D, int speed_circular) //Clockwise
 	while(1)
 	{
 		ANG_VAL = read_ang();
-		printf("ANG_VAL=%f\n");
+		printf("ANG_VAL=%f\n",ANG_VAL);
 		if(D=='R' && ANG_VAL>(final_angle-thresh_angle)) {
 			stop_car();
 			gettimeofday(&stopDate,NULL);
