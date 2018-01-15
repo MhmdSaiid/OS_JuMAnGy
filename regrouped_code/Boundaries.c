ce98b3929f5cc3859e8762c225ad8c6b0bd86955
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-15 12:09:15 +0100

    debug

--- regrouped_code/Boundaries.c
+++ regrouped_code/Boundaries.c
@@ -67,10 +67,10 @@ start_boundary_y = end_boundary_y;
       if( abs(y_position-initial_position_y)<20 ) { //returned to initial position
         if(relative_angle<0) rotate_car(relative_angle,'L',SPEED_CIRCULAR);
         else rotate_car(relative_angle,'R',SPEED_CIRCULAR);
-	return;
+				return;
   		}
-      start_boundary_x=(US_VAL/10*cos((relative_angle+90.0)*val)+x_position); // We are to the right of the boundary
-      start_boundary_y=(y_position +(US_VAL/10*sin((relative_angle+90.0)*val)));
+      start_boundary_x=end_boundary_x; // We are to the right of the boundary
+      start_boundary_y=end_boundary_y;
 			move(SPEED_LINEAR,1000,0,'F');
 
 			new_x=x_position;
@@ -80,11 +80,14 @@ start_boundary_y = end_boundary_y;
 
       rotate_car(90.0,'R',SPEED_CIRCULAR);
 			if(read_US()<200){ //if the boundary is still there
+				printf("OBSTACLE IS STILL HERE!!\n",);
         US_VAL = read_US();
-        end_boundary_x=(US_VAL/10*cos(relative_angle*val)+x_position);//The boundary is definetly still here so we draw it form the start to the end
+        end_boundary_x=((US_VAL/10*cos(relative_angle*val))+x_position);//The boundary is definetly still here so we draw it form the start to the end
         end_boundary_y=(y_position +(US_VAL/10*sin(relative_angle*val)));
         add_bound_line(&boundaries,start_boundary_x,start_boundary_y,end_boundary_x,end_boundary_y);
-        rotate_car(angle,'L',SPEED_CIRCULAR);
+				start_boundary_x=end_boundary_x; // We are to the right of the boundary
+				start_boundary_y=end_boundary_y;
+				rotate_car(angle,'L',SPEED_CIRCULAR);
 			}
 			else {
 				//rotate_car(10.0, 'R',SPEED_CIRCULAR);
@@ -108,10 +111,10 @@ start_boundary_y = end_boundary_y;
 	        new_x=x_position;
 	        new_y=y_position;
     			printf("add line: %f, %f, into %f, %f", former_x, former_y, new_x, new_y);
-	        add_big_line_of(map, (int)(floor(former_x)), (int)(floor(former_y)), (int)(floor(new_x)), (int)(floor(new_y)),10, EMPTY);/*update the map with the places we explored without meeting an obstacle*/
+	        //add_big_line_of(map, (int)(floor(former_x)), (int)(floor(former_y)), (int)(floor(new_x)), (int)(floor(new_y)),10, EMPTY);/*update the map with the places we explored without meeting an obstacle*/
 				}
 				find_right_angle_obst();
-        end_boundary_x=(US_VAL/10*cos(relative_angle*val)+x_position);
+        end_boundary_x=((US_VAL/10*cos(relative_angle*val))+x_position);
         end_boundary_y=(y_position +(US_VAL/10*sin(relative_angle*val)));
         add_bound_line(&boundaries,start_boundary_x,start_boundary_y,end_boundary_x,end_boundary_y); //update the map with the positions of the boundary
 				rotate_car(angle, 'L', SPEED_CIRCULAR);
@@ -120,8 +123,8 @@ start_boundary_y = end_boundary_y;
 		US_VAL = read_US();
 		printf("US_VAL = %f\n",US_VAL);
 		}
-    start_boundary_x=(US_VAL/10*cos(relative_angle*val)+x_position); //We are at an angle so the next boundary is in front
-    start_boundary_y=(y_position +(US_VAL/10*sin(relative_angle*val)));
+    //start_boundary_x=(US_VAL/10*cos(relative_angle*val)+x_position); //We are at an angle so the next boundary is in front
+    //start_boundary_y=(y_position +(US_VAL/10*sin(relative_angle*val)));
 		rotate_car(angle,'R',SPEED_CIRCULAR);//get awaay from the corner so turn to the charted obstacle move back then 180
 		move(SPEED_LINEAR,timeToStop*2,0,'B');
 		rotate_car(180.0,'L',SPEED_CIRCULAR);
